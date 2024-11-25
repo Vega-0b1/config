5aa6d14b7c61d8847ea08d1c0868bb25b085503a
jcvega <jcvega0b1@gmail.com>
2024-11-25 08:37:19 -0800

    sync

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,7 +9,7 @@ config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = false
 config.window_decorations = "RESIZE"
 
-config.color_scheme = 'Dracula (Official)'
+config.color_scheme = 'Catppuccin Macchiato'
 
 
 return config
