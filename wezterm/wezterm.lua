0fe53770a334e16f1febc39f2e06cee11a318b7b
jcvega <jcvega0b1@gmail.com>
2024-11-25 08:15:38 -0800

    fixed wez

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,15 @@
+
+local wezterm = require 'wezterm'
+
+
+local config = wezterm.config_builder()
+
+
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
+config.window_decorations = "RESIZE"
+
+config.color_scheme = 'Dracula (Official)'
+
+
+return config
