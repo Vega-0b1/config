f3581f82c9ab688b06846d718d635dded5b72924
jcvega <jcvega0b1@gmail.com>
2024-12-09 11:58:46 -0800

    leaving the light theme and trying out other themes

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,7 +5,7 @@ local config = wezterm.config_builder()
 config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = false
 config.window_decorations = "RESIZE"
-config.color_scheme = 'Catppuccin Latte'
+config.color_scheme = 'dracula'
 
 config.keys = {
   {
