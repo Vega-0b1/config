a9ef28fcd669f7dec9b3fea6ac544a3473e0f204
jcvega <jcvega0b1@gmail.com>
2024-12-26 08:47:39 -0800

    added script to hypridle to fix workspace assignments if dpms takes too long

---
+++ hypr/scripts/monitor_setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sleep 5
+
+hyprctl dispatch moveworkspacetomonitor 1 DP-1
+hyprctl dispatch moveworkspacetomonitor 2 DP-1
+hyprctl dispatch moveworkspacetomonitor 3 DP-1
+hyprctl dispatch moveworkspacetomonitor 4 DP-2
