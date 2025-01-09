34ea8b9af79ef0e0e42b8ef3da15d96e0350bbfc
jcvega <jcvega0b1@gmail.com>
2025-01-09 12:41:55 -0800

    forgot to add kill instance

--- hypr/scripts/wofi_brightness_notification.sh
+++ hypr/scripts/wofi_brightness_notification.sh
@@ -14,3 +14,9 @@ PERCENTAGE=$((BRIGHTNESS * 100 / MAX_BRIGHTNESS))
 
 # Display brightness level with Wofi
 echo "🔆 Brightness: $PERCENTAGE%" | wofi --dmenu --lines=1 --width=200
+
+WOFI_PID=$!
+
+# Close Wofi after 2 seconds
+sleep 2
+kill "$WOFI_PID" 2>/dev/null
