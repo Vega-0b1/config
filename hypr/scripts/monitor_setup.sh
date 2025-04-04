4debbb43fea4cc0941af8ffbe0d94f88c7cc9a30
jcvega <jcvega0b1@gmail.com>
2025-04-04 16:48:46 -0700

    autolaunch script and kanata setups scripts added

--- hypr/scripts/monitor_setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sleep 5
-
-hyprctl dispatch moveworkspacetomonitor 1 DP-1
-hyprctl dispatch moveworkspacetomonitor 2 DP-1
-hyprctl dispatch moveworkspacetomonitor 3 DP-1
-hyprctl dispatch moveworkspacetomonitor 4 DP-2
