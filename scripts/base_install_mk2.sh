81da8f4ffd095bdb69fb4a3688f5c61d0ce50ca6
jcvega <jcvega0b1@gmail.com>
2025-08-23 10:12:21 -0700

    further tweaks

--- scripts/base_install_mk2.sh
+++ scripts/base_install_mk2.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 #chsh -s $(which zsh) $USER
-echo "installing main packages"
 set -e
 pacman -Syu \
 sddm \
+hyprland \
 thunar \
 thunar-archive-plugin \
-thunar-volman \ bash-language-server \
+thunar-volman \
+bash-language-server \
 brightnessctl \
 hyprpicker \
 btop \
 neovim \
-kitty \
-freecad \
 ttf-meslo-nerd \
-cliphist \
 poppler \
 jq \
 fd \
@@ -28,36 +26,27 @@ zsh \
 zsh-syntax-highlighting \
 zsh-autosuggestions \
 starship \
-hyprland \
 hypridle \
-waybar \
-grim \
-slurp \
-dunst \
 wofi \
 wl-clipboard \
-blueman \
-network-manager-applet \
+libgtop \
+dart-sass \
+gvfs \
+gtksourceview3 \
+libsoup3 \
 yazi  \
 --noconfirm
-echo "done"
 
 echo "setting up git defaults"
 git config --global user.name "jcvega"
 git config --global user.email "jcvega0b1@gmail.com"
 git config --global credential.helper store
-echo "done"
 
-echo "enabling bluetooth"
 systemctl start bluetooth
 systemctl enable bluetooth
-echo "done"
 
 export EDITOR=nvim
-cp /home/jcvega/.config/zsh/.zshrc /home/jcvega
-cp /home/jcvega/.config/scripts/kanata.service /etc/systemd/system
+sudo cp /home/jcvega/.config/zsh/.zshrc /home/jcvega
+sudo cp /home/jcvega/.config/scripts/kanata.service /etc/systemd/system
 systemctl enable sddm
 systemctl start sddm
-echo "running yay install"
-./kanata_setup.sh
-echo "yay is done
