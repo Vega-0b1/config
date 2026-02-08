2b51a5d3327a8838368507b94e033a5133b5d00b
jcvega <jcvega0b1@gmail.com>
2026-02-07 17:51:32 -0800

    moving config root up a directory to start at home folder

--- closet/zsh/plugins.zsh
+++
@@ -1,6 +0,0 @@
-# Autosuggestions
-source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
-
-# Must be sourced **last**
-source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-
--- nvim/lua/plugins/notify.lua
+++
@@ -1,9 +0,0 @@
-
-return {
-  "rcarriga/nvim-notify",
-  opts = {
-    stages = "static", -- disables animations
-    timeout = 800,     -- adjust
-    fps = 60,          -- optional; irrelevant with static
-  },
-}
