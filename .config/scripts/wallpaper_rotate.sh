bcdd4ab53fe45da80d181295f6cee281235d239f
jcvega <jcvega0b1@gmail.com>
2026-04-28 15:11:13 -0700

    simplified window rules for audio controller and network manager

--- .config/scripts/wallpaper_rotate.sh
+++ .config/scripts/wallpaper_rotate.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 WALLPAPER_DIR="$HOME/.config/wallpaper"
-INTERVAL=900  # 15 minutes in seconds
+INTERVAL=900 # 15 minutes in seconds
 
 get_random_wallpaper() {
-    find "$WALLPAPER_DIR" -type f \( -name "*.jpg" -o -name "*.png" -o -name "*.jpeg" \) | shuf -n 1
+  find "$WALLPAPER_DIR" -type f \( -name "*.jpg" -o -name "*.png" -o -name "*.jpeg" \) | shuf -n 1
 }
 
 set_wallpaper() {
-    local wallpaper="$1"
-    swww img "$wallpaper" --transition-type wipe --transition-duration 1
+  local wallpaper="$1"
+  swww img "$wallpaper" --transition-type simple --transition-duration 1
 }
 
 # Initial wallpaper
@@ -17,6 +17,6 @@ set_wallpaper "$(get_random_wallpaper)"
 
 # Rotate every 15 minutes
 while true; do
-    sleep $INTERVAL
-    set_wallpaper "$(get_random_wallpaper)"
+  sleep $INTERVAL
+  set_wallpaper "$(get_random_wallpaper)"
 done
